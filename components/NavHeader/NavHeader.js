78279ca472619c9f1eda5e9517a6374e125878d2
CrazyCatZhang <CrazyCatZhang@outlook.com>
2023-03-27 23:50:01 +0800

    NavHeader Component

---
+++ components/NavHeader/NavHeader.js
@@ -0,0 +1,29 @@
+// components/NavHeader.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+    nav: {
+      type: String,
+      value: 'CatZhang'
+    }
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  },
+  options: {
+    styleIsolation: 'shared'
+  }
+})
