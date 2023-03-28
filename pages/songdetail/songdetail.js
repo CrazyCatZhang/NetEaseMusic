93bcd26e43fbd9dce167df77bbfa1558b8f1288c
CrazyCatZhang <CrazyCatZhang@outlook.com>
2023-03-28 16:48:39 +0800

    Drag the progress bar to play

--- pages/songdetail/songdetail.js
+++ pages/songdetail/songdetail.js
@@ -5,6 +5,8 @@ import moment from 'moment'
 
 const appInstance = getApp()
 
+const max = 250
+
 Page({
     /**
      * 页面的初始数据
@@ -15,7 +17,10 @@ Page({
         musicLink: '',
         currentTime: '00:00',
         durationTime: '',
-        percentage: 0
+        percentage: 0,
+        slotLeft: 0,
+        startPoint: {},
+        currentSeconds: 0
     },
 
     handleMusicPlay() {
@@ -74,11 +79,49 @@ Page({
         })
     },
 
+    slotMove(e) {
+        let endPoint = e.touches[e.touches.length - 1]
+        let translateX = endPoint.clientX - this.data.startPoint.clientX
+        let slotLeft = this.data.slotLeft + translateX
+        if (slotLeft > max) {
+            slotLeft = max
+        }
+        if (slotLeft < 0) {
+            slotLeft = 0
+        }
+        const currentSeconds = ((this.data.percentage / 100) * this.data.song.dt) / 1000
+        const currentTime = moment(currentSeconds * 1000).format('mm:ss')
+        this.setData({
+            startPoint: endPoint,
+            slotLeft,
+            percentage: parseInt((slotLeft / max) * 100),
+            currentTime,
+            currentSeconds
+        })
+    },
+
+    slotStart(e) {
+        this.backgroundAudioManager.pause()
+        this.setData({
+            startPoint: e.touches[0]
+        })
+    },
+
+    slotEnd() {
+        this.backgroundAudioManager.seek(this.data.currentSeconds)
+        const { song, musicLink } = this.data
+        if (!musicLink) {
+            this.musicControl(true, song.id, musicLink)
+            this.backgroundAudioManager.startTime = this.data.currentSeconds
+        } else {
+            this.backgroundAudioManager.play()
+        }
+    },
+
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
-        console.log(1111)
         const { musicId } = options
         this.getMusicInfo(musicId)
 
@@ -100,12 +143,13 @@ Page({
             this.changePlayState(false)
         })
         this.backgroundAudioManager.onTimeUpdate(() => {
-            if (appInstance.globalData.isMusicPlay && appInstance.globalData.musicId === +musicId) {
+            if (appInstance.globalData.isMusicPlay && appInstance.globalData.musicId === this.data.song.id) {
                 let currentTime = moment(this.backgroundAudioManager.currentTime * 1000).format('mm:ss')
                 let percentage = (this.backgroundAudioManager.currentTime / this.backgroundAudioManager.duration) * 100
                 this.setData({
                     currentTime,
-                    percentage
+                    percentage,
+                    slotLeft: (percentage / 100) * max
                 })
             }
         })
